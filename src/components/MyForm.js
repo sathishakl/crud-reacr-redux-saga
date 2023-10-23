f3ad1ac5f7b8b9b64c6ba0c9a5b78c255bd77fc8
Sathish Raja <msathis.akl@hotmail.com>
2023-10-23 15:38:04 +0800

    Added Form

--- src/components/MyForm.js
+++ src/components/MyForm.js
@@ -1,5 +1,44 @@
+import { Container, Input, Button } from "@mui/material";
+import { useState } from "react";
+
 const MyForm = () => {
-  return <h1>MyForm</h1>;
+  const [user, setuser] = useState({
+    id: 0,
+    name: "",
+    email: "",
+    password: "",
+  });
+  const handleChange = (prop) => (event) => {
+    setuser({ ...user, [prop]: event.target.value });
+  };
+  return (
+    <>
+      <Container>
+        <Input value={user.id} fullWidth disable />
+        <Input
+          onChange={() => handleChange("name")}
+          placeholder="Enter Name"
+          value={user.name}
+          fullWidth
+        />
+        <Input
+          onChange={() => handleChange("email")}
+          placeholder="Enter Email"
+          value={user.email}
+          fullWidth
+        />
+        <Input
+          onChange={() => handleChange("password")}
+          placeholder="Enter Password"
+          value={user.password}
+          fullWidth
+        />
+        <Button fullWidth varient="contained">
+          Submit
+        </Button>
+      </Container>
+    </>
+  );
 };
 
 export default MyForm;
